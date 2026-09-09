7e5a55af5678f5be68cef3826b75019b7c02c51e
Ekram-ekiti <ekru6482@gmail.com>
2026-09-09 22:28:18 +0300

    error fixed

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
   console.log(err.name, err.message);
-  process.exit(1);
 });
 
 const dotenv = require("dotenv");
@@ -22,16 +21,14 @@ mongoose.connect(DB).then(() => {
 //   console.log('Database connection failed  💥:');
 // });
 const port = process.env.PORT;
-const server = app.listen(port, () => {
-  console.log(`App is running on ${port}...`);
-});
+if (process.env.NODE_ENV === "development") {
+  app.listen(port, () => {
+    console.log(`App is running on ${port}...`);
+  });
+}
 
 process.on("unhandledRejection", (err) => {
   console.log("UNHANDLED REJECTION! 💥 Shutting down gracefully...");
   console.log(`Error Name: ${err.name}`);
   console.log(`Error Message: ${err.message}`);
-
-  server.close(() => {
-    process.exit(1);
-  });
 });
